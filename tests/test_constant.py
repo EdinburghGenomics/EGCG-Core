4986419371caf612b8f7adc1a8d2bf4153287572
tcezard <timothee.cezard@ed.ac.uk>
2017-10-30 11:45:05 +0000

    refactor test

--- tests/test_constant.py
+++ tests/test_constant.py
@@ -5,7 +5,4 @@ def test_constants_uniqueness():
     constants = [item for item in dir(cst) if not item.startswith("__")]
     uniq_constants = set()
     for constant in constants:
-        if constant in uniq_constants:
-            assert False
-        uniq_constants.add(constant)
-    assert True
+        assert constant not in uniq_constants
